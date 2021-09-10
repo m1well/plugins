ea46ca542c026b01206f298718f11a955ebc27a6
David Wertheimer <dwertheimer@users.noreply.github.com>
2021-09-09 22:44:33 -0700

    WIP hasChildren tests

--- dwertheimer.TaskAutomations/__tests__/paragraphDetail.test.js
+++ dwertheimer.TaskAutomations/__tests__/paragraphDetail.test.js
@@ -1,3 +1,4 @@
+import { isNullableTypeAnnotation } from '@babel/types'
 import * as pd from '../src/paragraphDetail'
 const _ = require('lodash')
 /* eslint-disable */
@@ -12,13 +13,13 @@ const noteWithTasks = {
     { type: 'title', indents: 0, heading: '', headingLevel: 2, content: 'heading2' },
     { type: 'scheduled', indents: 0, heading: 'heading2', headingLevel: 2, content: 'sked >2021-09-11' },
     { type: 'open', indents: 1, heading: 'heading2', headingLevel: 2, content: 'open2' },
-    { type: 'text', indents: 0, heading: 'heading2', headingLevel: 2, content: '	text under bullet' },
+    { type: 'text', indents: 0, heading: 'heading2', headingLevel: 2, content: '\ttext under bullet' },
     { type: 'text', indents: 0, heading: 'heading2', headingLevel: 2, content: '---' },
     { type: 'title', indents: 0, heading: '', headingLevel: 1, content: 'heading1again' },
     { type: 'open', indents: 0, heading: 'heading2again', headingLevel: 1, content: 'not indented' },
     { type: 'open', indents: 1, heading: 'heading2again', headingLevel: 1, content: 'indented' },
     { type: 'open', indents: 2, heading: 'heading2again', headingLevel: 1, content: 'bullet under' },
-    { type: 'text', indents: 0, heading: 'heading2again', headingLevel: 1, content: '		text under indented' },
+    { type: 'text', indents: 0, heading: 'heading2again', headingLevel: 1, content: '\t\t\ttext under indented' },
     { type: 'text', indents: 0, heading: 'heading2again', headingLevel: 1, content: '	text outside' },
     { type: 'text', indents: 0, heading: 'heading2again', headingLevel: 1, content: '#aTag' },
     { type: 'open', indents: 0, heading: 'heading2again', headingLevel: 1, content: 'something under a tag' },
@@ -62,21 +63,7 @@ describe('paragraphDetail', () => {
     const metaAfterHeadingSet = pd.setHeadings(_.cloneDeep(noteWithTasks).paragraphs, meta)
     expect(metaAfterHeadingSet[1].heading).toEqual(noteWithTasks.paragraphs[0].content)
   })
-  test('dwertheimer.TaskAutomations - paragraphDetail.flagParagraphsForSweeping', () => {
-    const metaAfterSweeping = pd.flagParagraphsForSweeping(_.cloneDeep(noteWithTasks).paragraphs)
-    expect(metaAfterSweeping[0].sweep).toBe(false)
-    expect(metaAfterSweeping[15].sweep).toBe(true)
-    expect(metaAfterSweeping[3].heading).toBe('TestTitle')
-  })
 
-  test('dwertheimer.TaskAutomations - paragraphDetail.flagParagraphsForSweeping do not include Note Title', () => {
-    let meta = pd.initializeMetaData(noteWithTasks.paragraphs)
-    const afterSweeping2 = pd.flagParagraphsForSweeping(_.cloneDeep(noteWithTasks).paragraphs, { includeTitle: false })
-    expect(afterSweeping2[0].sweep).toBe(false)
-    expect(afterSweeping2[15].sweep).toBe(true)
-    console.log(afterSweeping2[3])
-    expect(afterSweeping2[3].heading).toBe('')
-  })
   test('dwertheimer.TaskAutomations - paragraphDetail.setTextIndents index count is correct', () => {
     const textCheckText = {
       paragraphs: [
@@ -95,6 +82,80 @@ describe('paragraphDetail', () => {
   })
 })
 
+test('dwertheimer.TaskAutomations - paragraphDetail.countLeadingTabs test leading tab count', () => {
+  expect(pd.countLeadingTabs(``)).toBe(0)
+  expect(pd.countLeadingTabs(`\t`)).toBe(1)
+  expect(pd.countLeadingTabs(`\t\t\tText here`)).toBe(3)
+})
+
+test('dwertheimer.TaskAutomations - paragraphDetail.hasChildren test with strings', () => {
+  expect(pd.hasChildren({ indents: 0 }, { indents: 1 })).toBe(true)
+  expect(pd.hasChildren({ indents: 1 }, { indents: 0 })).toBe(false)
+  expect(pd.hasChildren({ indents: 0 }, { indents: 0 })).toBe(false)
+  expect(pd.hasChildren({ indents: 2 }, { indents: 2 })).toBe(false)
+  expect(pd.hasChildren({ indents: 4 }, { indents: 2 })).toBe(false)
+})
+
+test('dwertheimer.TaskAutomations - paragraphDetail.comparePredecessor meta.hasChildren test with strings', () => {
+  expect(pd.comparePredecessor({}, [{ indents: 0 }, { indents: 1 }], 0)[0].hasChildren).toBe(true)
+  expect(pd.comparePredecessor({}, [{ indents: 0 }, { indents: 1 }], 1)[1].hasChildren).toBe(false)
+  expect(pd.comparePredecessor({}, [{ indents: 1 }, { indents: 0 }], 0)[0].hasChildren).toBe(false)
+})
+
+test('dwertheimer.TaskAutomations - paragraphDetail.comparePredecessor meta.hasChildren test with paragraph data', () => {
+  let lastMeta = null
+  let meta = pd.initializeMetaData(noteWithTasks.paragraphs)
+  meta = pd.setTextIndents(noteWithTasks.paragraphs, meta)
+  const eachPara = noteWithTasks.paragraphs.map((p, j) => pd.comparePredecessor(noteWithTasks.paragraphs[j], meta, j))
+  // each run sends back the entire array of meta data
+  expect(eachPara[3][3].hasChildren).toBe(false)
+  expect(eachPara[4][4].hasChildren).toBe(true)
+  expect(eachPara[9][9].hasChildren).toBe(true)
+  expect(eachPara[10][10].hasChildren).toBe(true)
+  expect(eachPara[11][11].hasChildren).toBe(true)
+  expect(eachPara[eachPara.length - 1][eachPara.length - 1].hasChildren).toBe(false)
+})
+
+test('dwertheimer.TaskAutomations - paragraphDetail.flagParagraphsForSweeping', () => {
+  const metaAfterSweeping = pd.flagParagraphsForSweeping(_.cloneDeep(noteWithTasks).paragraphs)
+  expect(metaAfterSweeping[0].sweep).toBe(false)
+  expect(metaAfterSweeping[15].sweep).toBe(true)
+  expect(metaAfterSweeping[3].heading).toBe('TestTitle')
+})
+
+test('dwertheimer.TaskAutomations - paragraphDetail.flagParagraphsForSweeping do not include Note Title', () => {
+  let meta = pd.initializeMetaData(noteWithTasks.paragraphs)
+  const afterSweeping2 = pd.flagParagraphsForSweeping(_.cloneDeep(noteWithTasks).paragraphs, { includeTitle: false })
+  expect(afterSweeping2[0].sweep).toBe(false)
+  expect(afterSweeping2[15].sweep).toBe(true)
+  expect(afterSweeping2[3].heading).toBe('')
+})
+
+// FOR REFERENCE:
+// let output = ''
+// noteWithTasks.paragraphs.forEach((n, i) => {
+//   output += `noteWithTasks.paragraphs[${i}] = ${JSON.stringify(noteWithTasks.paragraphs[i])}\n`
+// })
+// console.log(output)
+/*
+noteWithTasks.paragraphs[0] = {"type":"title","indents":0,"heading":"","headingLevel":1,"content":"TestTitle"}
+noteWithTasks.paragraphs[1] = {"type":"open","indents":0,"heading":"TestTitle","headingLevel":1,"content":"open1"}
+noteWithTasks.paragraphs[2] = {"type":"list","indents":0,"heading":"TestTitle","headingLevel":1,"content":"bullet"}
+noteWithTasks.paragraphs[3] = {"type":"title","indents":0,"heading":"","headingLevel":2,"content":"heading2"}
+noteWithTasks.paragraphs[4] = {"type":"scheduled","indents":0,"heading":"heading2","headingLevel":2,"content":"sked >2021-09-11"}
+noteWithTasks.paragraphs[5] = {"type":"open","indents":1,"heading":"heading2","headingLevel":2,"content":"open2"}
+noteWithTasks.paragraphs[6] = {"type":"text","indents":0,"heading":"heading2","headingLevel":2,"content":"\ttext under bullet"}
+noteWithTasks.paragraphs[7] = {"type":"text","indents":0,"heading":"heading2","headingLevel":2,"content":"---"}
+noteWithTasks.paragraphs[8] = {"type":"title","indents":0,"heading":"","headingLevel":1,"content":"heading1again"}
+noteWithTasks.paragraphs[9] = {"type":"open","indents":0,"heading":"heading2again","headingLevel":1,"content":"not indented"}
+noteWithTasks.paragraphs[10] = {"type":"open","indents":1,"heading":"heading2again","headingLevel":1,"content":"indented"}
+noteWithTasks.paragraphs[11] = {"type":"open","indents":2,"heading":"heading2again","headingLevel":1,"content":"bullet under"}
+noteWithTasks.paragraphs[12] = {"type":"text","indents":0,"heading":"heading2again","headingLevel":1,"content":"\t\t  text under indented"}
+noteWithTasks.paragraphs[13] = {"type":"text","indents":0,"heading":"heading2again","headingLevel":1,"content":"\ttext outside"}
+noteWithTasks.paragraphs[14] = {"type":"text","indents":0,"heading":"heading2again","headingLevel":1,"content":"#aTag"}
+noteWithTasks.paragraphs[15] = {"type":"open","indents":0,"heading":"heading2again","headingLevel":1,"content":"something under a tag"}
+noteWithTasks.paragraphs[16] = {"type":"empty","indents":0,"heading":"heading2again","headingLevel":1,"content":""}
+*/
 // MARKDOWN FOR noteWithTasks
 const noteWithTasksMD = `# TestTitle
 * open1
@@ -108,7 +169,8 @@ const noteWithTasksMD = `# TestTitle
 * not indented
 	* indented
 		* bullet under
-		text under indented
+      text under indented
 	text outside
 #aTag
-* something under a tag`
+* something under a tag
+[empty]`
--- dwertheimer.TaskAutomations/src/paragraphDetail.js
+++ dwertheimer.TaskAutomations/src/paragraphDetail.js
@@ -22,47 +22,80 @@ export function setHeadings(paragraphs: [TParagraph], meta: [Metadata]): [Metada
   return meta
 }
 
+export function countLeadingTabs(contentStr: string): number {
+  let numOfTabs = 0,
+    start = 0
+  const content = contentStr.replace(/	/g, '\t')
+  while (content.charAt(start++) === '\t') numOfTabs++
+  return numOfTabs
+}
+
 /**
  * @description Adds the indents of tabs to text items
  * @param {[TParagraph]} paragraphs
  * @param {[Metadata]} meta - extra paragraph metadata (b/c paragraphs is readonly) - tracks paragraphs
  * @returns {[Metadata]} meta
  */
-export function setTextIndents(paragraphs: [TParagraph], meta: [Metadata]): [Metadata] {
+export function setTextIndents(paragraphs: [TParagraph], meta: [Metadata]): Array<Metadata> {
+  const newMeta = meta.slice()
   const paras = paragraphs.forEach((p, i) => {
     if (p.type === 'text' && p.content.length) {
-      let numOfTabs = 0,
-        start = 0
-      while (p.content.charAt(start++) === '\t') numOfTabs++
-      meta[i].indents = numOfTabs
+      newMeta[i].indents = countLeadingTabs(p.content)
     }
   })
-  return meta
+  return newMeta
+}
+
+// Look to find children (assumes we are going backwards from the last paragraph)
+export function hasChildren(metaValue: Metadata, prevMetaValue: Metadata): boolean {
+  if (prevMetaValue) {
+    if (prevMetaValue.indents > metaValue.indents) {
+      return true
+    }
+  }
+  return false
 }
 
 /**
  * When going backwards from finding an open task, look for headings/context that goes with the task
  * @param {TParagraph} paragraph
  * @param {} lastSwept
- * @returns {string} null | 'heading' | 'reset'
+ * @returns {Metadata} meta
  *
  */
-export function comparePredecessor(paragraph, lastSwept) {
-  //FIXME: need coverage
-  const p = lastSwept.paragraph
-  if (paragraph.type === 'title') {
-    if (paragraph.headingLevel < lastSwept.headingLevel) return 'heading'
-  } else {
-    if (paragraph.headingLevel >= lastSwept.headingLevel) return 'reset'
-  }
-  if (paragraph.type === 'text' && p.content === '---') return 'separator'
-  if (paragraph.type === 'text' && p.content === '---') return 'text'
-  return null
+export function comparePredecessor(paragraph: TParagraph, meta: [Metadata], metaIndex: number): [Metadata] {
+  //TODO FIXME: need typing but need strategy even more. What are we doing here?
+  const thisMeta = meta[metaIndex]
+  const prevMetaValue = meta[metaIndex + 1] || null
+  // console.log(`${metaValue.index}: indents: ${metaValue.indents} ${prevMetaValue?.indents} "${paragraph.content}"`)
+  thisMeta.hasChildren = hasChildren(thisMeta, prevMetaValue)
+  // if (paragraph.type === 'title') {
+  //   if (paragraph.headingLevel < lastSwept.headingLevel) return 'heading'
+  // } else {
+  //   if (paragraph.headingLevel >= lastSwept.headingLevel) return 'reset'
+  // }
+  // if (paragraph.type === 'text' && p.content === '---') return 'separator'
+  // if (paragraph.type === 'text' && p.content === '---') return 'text'
+  return meta
 }
 
-type Metadata = { indents: number, headingLevel: number, heading: string, sweep: boolean }
-export const initializeMetaData = (paragraphs): [Metadata] =>
-  paragraphs.map((p) => ({ indents: p.indents, headingLevel: p.headingLevel, heading: p.heading, sweep: false }))
+type Metadata = {
+  indents: number,
+  headingLevel: number,
+  heading: string,
+  sweep: boolean,
+  index: number,
+  hasChildren: boolean,
+}
+export function initializeMetaData(paragraphs: [TParagraph]): [Metadata] {
+  return paragraphs.map((p, i) => ({
+    indents: p.indents,
+    headingLevel: p.headingLevel,
+    heading: p.heading,
+    sweep: false,
+    index: i,
+  }))
+}
 
 /**
  * @description Set the .sweep flag on paragraphs to be swept
@@ -85,15 +118,17 @@ export function flagParagraphsForSweeping(paragraphs: [TParagraph], options: { [
     meta = setTextIndents(paragraphs, meta)
   }
   //loop through paragraphs in reverse order
+  let prevMeta = null
   for (let i = paragraphs.length - 1; i >= 0; i--) {
     const p = paragraphs[i]
-    const lastSwept = { paragraph: null, indents: 0 }
-    if (opts.ignore.includes(p.type)) continue
-    const sweepThis = lastSwept.paragraph && comparePredecessor(p, lastSwept)
-    if (isOpenTask(p) || sweepThis) {
-      meta[i].sweep = true
-      lastSwept.paragraph = p
-    }
+    // const lastSwept = { paragraph: null, indents: 0 }
+    // if (opts.ignore.includes(p.type)) continue
+
+    meta = comparePredecessor(p, meta, i)
+    // if (isOpenTask(p) || sweepThis) {
+    //   meta[i].sweep = true
+    //   lastSwept.paragraph = p
+    // }
   }
   return meta
 }
